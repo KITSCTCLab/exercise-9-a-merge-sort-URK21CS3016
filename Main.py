75dbb3892339381f8d9a2f6e45b8695db1c5c020
URK21CS3016 <109951881+URK21CS3016@users.noreply.github.com>
2022-10-17 15:45:03 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,41 +1,43 @@
-from typing import List
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
 
-def merge_sort(data) -> None:
-  if len(data)>1:
-        mid = len(data)//2
-        lefthalf = data[:mid]
-        righthalf = data[mid:]
 
-        merge_sort(lefthalf)
-        merge_sort(righthalf)
-        i=j=k=0       
-        while i < len(lefthalf) and j < len(righthalf):
-            if lefthalf[i] < righthalf[j]:
-                data[k]=lefthalf[i]
-                i=i+1
-            else:
-                data[k]=righthalf[j]
-                j=j+1
-            k=k+1
+def insert(root, new_value) -> BinaryTreeNode:
+    """If binary search tree is empty, make a new node, declare it as root and return the root.
+        If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
+        If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
+        Finally, return the root.
+        """
+    # Write your code here
 
-        while i < len(lefthalf):
-            data[k]=lefthalf[i]
-            i=i+1
-            k=k+1
 
-        while j < len(righthalf):
-            data[k]=righthalf[j]
-            j=j+1
-            k=k+1
+def inorder(root) -> None:
+    # Write your code here
+
+
+def preorder(root) -> None:
+    # Write your code here
+
+
+def postorder(root) -> None:
+    # Write your code here
 
 
 # Do not change the following code
 input_data = input()
-data = []
+flag = True
+root = None
 for item in input_data.split(', '):
-  if item.isnumeric():
-    data.append(int(item))
-  elif item.lstrip("-").isnumeric():
-    data.append(int(item))
-merge_sort(data)
-print(data)
+    if flag is True:
+        root = insert(None, int(item))
+        flag = False
+    else:
+        insert(root, int(item))
+inorder(root)
+print()
+preorder(root)
+print()
+postorder(root)
